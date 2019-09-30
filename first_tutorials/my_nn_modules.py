6270212ab830fceea872d543449292e5cb989cbb
erwulff <eric.g.t.wulff@gmail.com>
2019-09-30 11:05:44 +0200

    Added saved models and processed data.

---
+++ first_tutorials/my_nn_modules.py
@@ -0,0 +1,62 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+import numpy as np
+
+class AE_big(nn.Module):
+    def __init__(self, n_features=4):
+        super(AE_big, self).__init__()
+        self.en1 = nn.Linear(n_features, 8)
+        self.en2 = nn.Linear(8, 6)
+        self.en3 = nn.Linear(6, 4)
+        self.en4 = nn.Linear(4, 3)
+        self.de1 = nn.Linear(3, 4)
+        self.de2 = nn.Linear(4, 6)
+        self.de3 = nn.Linear(6, 8)
+        self.de4 = nn.Linear(8, n_features)
+        self.tanh = nn.Tanh()
+
+    def encode(self, x):
+        return self.tanh(self.en4(self.tanh(self.en3(self.tanh(self.en2(self.tanh(self.en1(x))))))))
+
+    def decode(self, x):
+        return self.de4(self.tanh(self.de3(self.tanh(self.de2(self.tanh(self.de1(x)))))))
+
+    def forward(self, x):
+        z = self.encode(x)
+        return self.decode(z)
+
+
+# Some helper functions
+def get_data(train_ds, valid_ds, bs):
+    return (
+        DataLoader(train_ds, batch_size=bs, shuffle=True),
+        DataLoader(valid_ds, batch_size=bs * 2),
+    )
+
+
+def loss_batch(model, loss_func, xb, yb, opt=None):
+    loss = loss_func(model(xb), yb)
+
+    if opt is not None:
+        loss.backward()
+        opt.step()
+        opt.zero_grad()
+
+    return loss.item(), len(xb)
+
+
+def fit(epochs, model, loss_func, opt, train_dl, valid_dl):
+    for epoch in range(epochs):
+        model.train()
+        for xb, yb in train_dl:
+            loss_batch(model, loss_func, xb, yb, opt)
+
+        model.eval()
+        with torch.no_grad():
+            losses, nums = zip(
+                *[loss_batch(model, loss_func, xb, yb) for xb, yb in valid_dl]
+            )
+        val_loss = np.sum(np.multiply(losses, nums)) / np.sum(nums)  # MSE-Loss
+        if(epoch % 1 == 0):
+            print('Epoch ' + str(epoch) + ':', 'Validation loss = ' + str(val_loss))
