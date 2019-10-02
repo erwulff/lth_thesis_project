a869f0683dbacadd4c89c2e7dfa88d7c76fff3d6
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-10-02 07:55:01 +0000

    Added ability to choose device in fit function

--- first_tutorials/my_nn_modules.py
+++ first_tutorials/my_nn_modules.py
@@ -26,13 +26,63 @@ class AE_big(nn.Module):
     def forward(self, x):
         z = self.encode(x)
         return self.decode(z)
+    
+
+class AE_big_2D_v1(nn.Module):
+    def __init__(self, n_features=4):
+        super(AE_big_2D_v1, self).__init__()
+        self.en1 = nn.Linear(n_features, 8)
+        self.en2 = nn.Linear(8, 6)
+        self.en3 = nn.Linear(6, 4)
+        self.en4 = nn.Linear(4, 2)
+        self.de1 = nn.Linear(2, 4)
+        self.de2 = nn.Linear(4, 6)
+        self.de3 = nn.Linear(6, 8)
+        self.de4 = nn.Linear(8, n_features)
+        self.tanh = nn.Tanh()
+
+    def encode(self, x):
+        return self.en4(self.tanh(self.en3(self.tanh(self.en2(self.tanh(self.en1(x)))))))
+
+    def decode(self, x):
+        return self.de4(self.tanh(self.de3(self.tanh(self.de2(self.tanh(self.de1(self.tanh(x))))))))
+
+    def forward(self, x):
+        z = self.encode(x)
+        return self.decode(z)
+    
+
+class AE_big_2D_v2(nn.Module):
+    def __init__(self, n_features=4):
+        super(AE_big_2D_v2, self).__init__()
+        self.en1 = nn.Linear(n_features, 8)
+        self.en2 = nn.Linear(8, 6)
+        self.en3 = nn.Linear(6, 4)
+        self.en4 = nn.Linear(4, 3)
+        self.en5 = nn.Linear(3, 2)
+        self.de1 = nn.Linear(2, 3)
+        self.de2 = nn.Linear(3, 4)
+        self.de3 = nn.Linear(4, 6)
+        self.de4 = nn.Linear(6, 8)
+        self.de5 = nn.Linear(8, n_features)
+        self.tanh = nn.Tanh()
+
+    def encode(self, x):
+        return self.en5(self.tanh(self.en4(self.tanh(self.en3(self.tanh(self.en2(self.tanh(self.en1(x)))))))))
+
+    def decode(self, x):
+        return self.de5(self.tanh(self.de4(self.tanh(self.de3(self.tanh(self.de2(self.tanh(self.de1(self.tanh(x))))))))))
+
+    def forward(self, x):
+        z = self.encode(x)
+        return self.decode(z)
 
 
 # Some helper functions
 def get_data(train_ds, valid_ds, bs):
     return (
-        DataLoader(train_ds, batch_size=bs, shuffle=True),
-        DataLoader(valid_ds, batch_size=bs * 2),
+        DataLoader(train_ds, batch_size=bs, shuffle=True, pin_memory=True),
+        DataLoader(valid_ds, batch_size=bs * 2, pin_memory=True),
     )
 
 
@@ -47,16 +97,18 @@ def loss_batch(model, loss_func, xb, yb, opt=None):
     return loss.item(), len(xb)
 
 
-def fit(epochs, model, loss_func, opt, train_dl, valid_dl):
+def fit(epochs, model, loss_func, opt, train_dl, valid_dl, device):
     for epoch in range(epochs):
         model.train()
         for xb, yb in train_dl:
+            xb = xb.to(device)
+            yb = yb.to(device)
             loss_batch(model, loss_func, xb, yb, opt)
 
         model.eval()
         with torch.no_grad():
             losses, nums = zip(
-                *[loss_batch(model, loss_func, xb, yb) for xb, yb in valid_dl]
+                *[loss_batch(model, loss_func, xb.to(device), yb.to(device)) for xb, yb in valid_dl]
             )
         val_loss = np.sum(np.multiply(losses, nums)) / np.sum(nums)  # MSE-Loss
         if(epoch % 1 == 0):
